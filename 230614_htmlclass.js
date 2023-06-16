3ac5bc2e5593b66ce34244892ef1898b214987f5
Hyeonmin072 <Hyeonmin072@users.noreply.github.com>
2023-06-16 12:40:13 +0900

    Create 230614_htmlclass.js

---
+++ 230614_htmlclass.js
@@ -0,0 +1,17 @@
+// window.onload=function(){
+//     let btn = document.querySelector('button');
+//     btn.onclick=function(){;   
+//         let play=document.getElementById("play");
+//         play.style.color="red";
+//         play.style.backgroundColor="blue"
+//         play.innerHTML = "<b>앙기말띠</b>";
+//         //play.textContent = "<b>앙기말띠</b>";
+        
+//     }
+// }
+
+
+    function change(){
+        var p =document.getElementById("myData");
+        p.innerHTML = "<h1>나는 김현민입니다.</h1>"; 
+    }
